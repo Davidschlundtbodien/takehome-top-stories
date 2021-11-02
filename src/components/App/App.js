56bfa133e48c5563cc95d578c7537672c80a1eae
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-11-02 11:14:10 -0600

    Add api call and hooks to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,18 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { Switch, Route} from 'react-router-dom';
+import { fetchTopStories } from '../../apiCalls';
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    fetchTopStories()
+    .then(data => setArticles(data.results))
+    .catch(err => setError(err))
+  }, [])
+
   return (
     <div className="App">
       <Switch>
