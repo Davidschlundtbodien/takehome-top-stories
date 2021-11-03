846203feef6b5c530f9e345490ebeb028c1c9549
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-11-03 14:00:14 -0600

    Fix bug on detailed refresh

--- src/components/ArticleDetailed/ArticleDetailed.js
+++ src/components/ArticleDetailed/ArticleDetailed.js
@@ -4,17 +4,21 @@ import './ArticleDetailed.css';
 const ArticleDetailed = ({ article }) => {
 
   return(
-    <article
-      className="article-container"
-      style={{backgroundImage: `url(${article.multimedia[0].url})`}}
-    >
-      <section className="article-content">
-        <p className="title">{article.title}</p>
-        <p className="abstract">{article.abstract}</p>
-        <p className="author">{article.byline}</p>
-        <a href={article.url}>Read Article</a>
-      </section>
-    </article>
+    <>
+      {article &&
+        <article
+          className="article-container"
+          style={{backgroundImage: `url(${article.multimedia[0].url})`}}
+        >
+          <section className="article-content">
+            <p className="title">{article.title}</p>
+            <p className="abstract">{article.abstract}</p>
+            <p className="author">{article.byline}</p>
+            <a href={article.url}>Read Article</a>
+          </section>
+        </article>
+      }
+    </>
   )
 }
 
